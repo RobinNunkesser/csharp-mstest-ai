e31565d1786a3d9b5e37eaa9b81be99978895f3f
nunkesser <robin.nunkesser@hshl.de>
2025-01-21 12:52:22 +0100

    CSP test

---
+++ AI.Tests/AI.Tests/Unit/Search/CSP/MapCspTests.cs
@@ -0,0 +1,33 @@
+using Italbytz.Adapters.Algorithms.AI.Search.CSP;
+using Italbytz.Adapters.Algorithms.AI.Search.CSP.Examples;
+using Italbytz.Adapters.Algorithms.AI.Search.CSP.Solver;
+using Italbytz.Ports.Algorithms.AI.Search.CSP;
+
+namespace AI.Tests.Unit.Search.CSP;
+
+[TestClass]
+public class MapCspTests
+{
+    private ICSP<Variable, string> csp;
+    
+    [TestInitialize]
+    public void TestInitialize()
+    {
+        csp = new MapCSP();
+    }
+    
+    [TestMethod]
+    public void TestStandardBackTrackingSearch()
+    {
+        var solver = new FlexibleBacktrackingSolver<Variable, string>();
+        var assignment = solver.Solve(csp);
+        Assert.IsNotNull(assignment);
+            Assert.AreEqual(MapCSP.BLUE, assignment.GetValue(MapCSP.WA));
+            Assert.AreEqual(MapCSP.RED, assignment.GetValue(MapCSP.NT));
+            Assert.AreEqual(MapCSP.GREEN, assignment.GetValue(MapCSP.SA));
+            Assert.AreEqual(MapCSP.BLUE, assignment.GetValue(MapCSP.Q));
+            Assert.AreEqual(MapCSP.RED, assignment.GetValue(MapCSP.NSW));
+            Assert.AreEqual(MapCSP.BLUE, assignment.GetValue(MapCSP.V));
+            Assert.AreEqual(MapCSP.RED, assignment.GetValue(MapCSP.T));
+    }   
+}
